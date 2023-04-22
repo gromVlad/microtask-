ed5448a7100c254751d535badc98a29e5c578b61
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-04-22 17:24:51 +0300

    react -router

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,45 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Todolist,TaskType} from './Todolist';
-import {v1} from 'uuid';
-
-export type FilterValuesType = "all" | "active" | "completed";
-
-export type ТodolistsType = {
-  id: string;
-  title: string;
-  filter: FilterValuesType;
-};
-
-
-export type Тodotask = {
-  [key: string]: TaskType[]
-};
-
+import React, { useEffect } from "react";
+import "./App.css";
+import { NavLink,Navigate,Route, Routes, useNavigate, useParams } from "react-router-dom";
 
 function App() {
+  
 
-    let todolistID1=v1();
-    let todolistID2=v1();
-
-    let [todolists, setTodolists] = useState<Array<ТodolistsType>>([
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
-    ])
-
-    let [tasks, setTasks] = useState<Тodotask>({
-      [todolistID1]: [
-        { id: v1(), title: "HTML&CSS", isDone: true },
-        { id: v1(), title: "JS", isDone: true },
-        { id: v1(), title: "ReactJS", isDone: false },
-        { id: v1(), title: "Rest API", isDone: false },
-        { id: v1(), title: "GraphQL", isDone: false },
-      ],
-      [todolistID2]: [
-        { id: v1(), title: "HTML&CSS2", isDone: true },
-        { id: v1(), title: "JS2", isDone: true },
-        { id: v1(), title: "ReactJS2", isDone: false },
-        { id: v1(), title: "Rest API2", isDone: false },
-        { id: v1(), title: "GraphQL2", isDone: false },
-      ],
-    });
-
-
-
-    function removeTask(id: string, idTodo:string) {
-      setTasks({...tasks,[idTodo]:tasks[idTodo].filter(el => el.id !== id)})
-    }
-
-    function addTask(title: string,idTodo:string) {
-      const newtask = { id: v1(), title, isDone: false }
-      setTasks({...tasks,[idTodo]:[...tasks[idTodo],newtask]})
-    }
-
-    function changeStatus(taskId: string, isDone: boolean, idTodo: string) {
-      setTasks({...tasks,[idTodo]:tasks[idTodo].map(el => el.id === taskId ? {...el,isDone}: el)})
-    }
-
-    function changeFilter(value: FilterValuesType, idTodo: string) {
-      setTodolists(
-        todolists.map((el) =>
-          el.id === idTodo ? { ...el, filter: value } : el
-        )
-      );
-    }
-
-
-    return (
-      <div className="App">
-        {todolists.map(el => {
-          let tasksForTodolist = tasks[el.id];
-
-          if (el.filter === "active") {
-            tasksForTodolist = tasks[el.id].filter((t) => t.isDone === false);
-          }
-          if (el.filter === "completed") {
-            tasksForTodolist = tasks[el.id].filter((t) => t.isDone === true);
-          }
-
-
-          return (
-            <Todolist
-              key={el.id}
-              id={el.id}
-              title={el.title}
-              tasks={tasksForTodolist}
-              removeTask={removeTask}
-              changeFilter={changeFilter}
-              addTask={addTask}
-              changeTaskStatus={changeStatus}
-              filter={el.filter}
-            />
-          );
-        })}
+  return (
+    <>
+      <div>
+        <NavLink to={"/"} style={(par) => { return {color:par.isActive ? 'blue' : 'black'}}}>main </NavLink>
+        <NavLink to={"/login"} className={({isActive}) => isActive ? 'act' : 'get' }> Login </NavLink>
+        <NavLink to={"/profile"}> profile </NavLink>
       </div>
-    );
+
+      <Routes>
+        {/* /* */}
+        <Route path={"/*"} element={<div>Error 404</div>} />
+        <Route path={"/"} element={<div>main</div>} />
+        <Route path={"/login"} element={<div>login</div>} />
+        <Route path={"/profile"} element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
+
+function Profile ()  {
+  const nav = useNavigate()
+
+  //тоесть при первом заспуске если юзер не зареган при попытке войти в профиль его перекидывает на login мол зарегайся дружок
+  useEffect( () => {
+    if (true) nav("/login");
+  },[])
+
+  return (
+    <div>
+      {/* 2 способ валидации логина или используем тернарный оператор */}
+      {true && <Navigate to={"/login"}/>}
+      profile
+      <button onClick={() => {nav("-1")}}>Logout</button>
+    </div>
+  )
+}
